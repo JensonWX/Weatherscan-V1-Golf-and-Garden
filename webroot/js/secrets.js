7d3314d6e5d87cfc241cae611333097f5c68f1c5
JensonWX <gameemail.1829@gmail.com>
2025-01-05 21:37:13 -0500

    js

---
+++ webroot/js/secrets.js
@@ -0,0 +1,6 @@
+var secrets = {
+    a_key: 'e1f10a1e78da46f5b10a1e78da96f525',
+    m_key: 'YOUR_API_KEY',
+    t_key: 'YOUR_TRAF_KEY',
+    bypass: '?bypass=visit discord.gg/weatherranch to get a bypass key'
+}
